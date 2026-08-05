f40881d062fba77ec52544ab6563e1948f7432b3
24pa1a5728-blip <24pa1a5728@vishnu.edu.in>
2026-08-05 10:50:00 +0530

    Add comparison functionality in Exercise9.js

---
+++ JavaScript Exercises Part 2/Exercise9.js
@@ -0,0 +1,7 @@
+let res = document.getElementById("res");
+
+document.getElementById("sbt").onclick = function() {
+    let num1 = Number(document.getElementById("input1").value);
+      let num2 = Number(document.getElementById("input2").value);
+    res.textContent = (num1 > num2 )? `${num1} is Greater` : `${num2} is  greater`;
+}
